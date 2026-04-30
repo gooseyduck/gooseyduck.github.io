5d02b131fdb124e0af351a3eb2c18088d0f28253
goosey <cja8658@gmail.com>
2026-04-30 19:11:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const data = [
   {
-    title: "Sunset Vibes",
-    desc: "Calm ocean sunset with warm tones.",
-    img: "https://images.unsplash.com/photo-1501973801540-537f08ccae7b"
+    title: "Signature Machine",
+    desc: "A recreation of the signature machine from Buckshot Roulette in blender.",
+    img: "https://raw.githubusercontent.com/gooseyduck/gooseyduck.github.io/refs/heads/main/Screenshot%202026-04-30%20190234.png"
   },
   {
-    title: "City Nights",
-    desc: "Neon-lit streets after rain.",
-    img: "https://images.unsplash.com/photo-1492684223066-81342ee5ff30"
+    title: "placeholder",
+    desc: "placeholder",
+    img: "https://media.discordapp.net/attachments/1485410934431416602/1499229227348787321/IMG_3429.jpg?ex=69f4b246&is=69f360c6&hm=f165606aa4660b790ce2466339677ea54cbd2f0395685d6cf6d203fbe6985481&=&format=webp&width=234&height=336"
   },
   {
-    title: "Mountain Air",
-    desc: "Fog rolling through quiet mountains.",
-    img: "https://images.unsplash.com/photo-1501785888041-af3ef285b470"
+    title: "placeholder",
+    desc: "placeholder",
+    img: "https://media.discordapp.net/attachments/1485410934431416602/1499229227348787321/IMG_3429.jpg?ex=69f4b246&is=69f360c6&hm=f165606aa4660b790ce2466339677ea54cbd2f0395685d6cf6d203fbe6985481&=&format=webp&width=234&height=336"
   }
 ];
 
